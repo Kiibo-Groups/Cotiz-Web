1207e9d89b286538b7ed0c52437eaf0097c2f6b1
Jair Quinto <tokinmym2@gmail.com>
2023-07-28 18:31:48 -0500

    2

--- app/Http/Controllers/Proveedor/CatalogoController.php
+++ app/Http/Controllers/Proveedor/CatalogoController.php
@@ -48,7 +48,7 @@ class CatalogoController extends Controller
 
 
         $providers = Rfc::where('id', auth()->user()->idempresa)->where('status', 0)->first();
-        dd($providers );
+        dd(auth()->user()->idempresa );
         return view($this->folder . 'catalogo.add', [
             'data' => new Services,
             'providers' => $providers
