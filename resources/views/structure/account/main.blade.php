ff998ac8ce93790e951735be1e49a633d71a31a9
Jair Quinto <tokinmym2@gmail.com>
2023-09-01 10:20:15 -0500

    /
    Signed-off-by: Jair Quinto <tokinmym2@gmail.com>

--- resources/views/structure/account/main.blade.php
+++ resources/views/structure/account/main.blade.php
@@ -3,16 +3,16 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<link href="{{ asset('/public/profile/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('/public/profile/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-<link href="{{ asset('profile/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-<link href="{{ asset('profile/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-<link href="{{ asset('profile/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-<link href="{{ asset('profile/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-<link href="{{ asset('profile/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
 <!-- tagify -->
 <link href="https://unpkg.com/@yaireo/tagify/dist/tagify.css" rel="stylesheet" type="text/css" />
 
 <!-- Template Main CSS File -->
-<link href="{{ asset('profile/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('/profile/css/style.css') }}" rel="stylesheet">
