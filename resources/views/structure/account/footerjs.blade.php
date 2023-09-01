782ada168576aed01f3505441b30d377df8a1984
Jair Quinto <tokinmym2@gmail.com>
2023-09-01 10:13:53 -0500

    /public
    Signed-off-by: Jair Quinto <tokinmym2@gmail.com>

--- resources/views/structure/account/footerjs.blade.php
+++ resources/views/structure/account/footerjs.blade.php
@@ -1,14 +1,14 @@
 <!-- Vendor JS Files -->
 
-<script src="{{ asset('/profile/vendor/jquery/jquery-3.5.1.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/chart.js/chart.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/echarts/echarts.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/quill/quill.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/simple-datatables/simple-datatables.js') }}"></script>
-<script src="{{ asset('/profile/vendor/tinymce/tinymce.min.js') }}"></script>
-<script src="{{ asset('/profile/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/jquery/jquery-3.5.1.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/chart.js/chart.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/quill/quill.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('/public/profile/vendor/php-email-form/validate.js') }}"></script>
 
 
 <!-- tagify -->
@@ -16,4 +16,4 @@
 <script src="https://unpkg.com/@yaireo/tagify@3.1.0/dist/tagify.polyfills.min.js"></script>
 
 <!-- Template Main JS File -->
-<script src="{{ asset('profile/js/main.js') }}"></script>
+<script src="{{ asset('/public/profile/js/main.js') }}"></script>
