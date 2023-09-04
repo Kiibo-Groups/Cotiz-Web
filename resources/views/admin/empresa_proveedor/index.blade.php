efe608c9f999367c04911133ccf4c2bab9bcf809
Jair Quinto <tokinmym2@gmail.com>
2023-09-04 14:19:41 -0500

    Empresa
    Signed-off-by: Jair Quinto <tokinmym2@gmail.com>

--- resources/views/admin/empresa_proveedor/index.blade.php
+++ resources/views/admin/empresa_proveedor/index.blade.php
@@ -21,75 +21,87 @@
                 <div class="col-lg-12">
                     @include('alerts')
                     <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">Listado de Empresas Proveedoras</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Listado de Empresas Proveedoras</h5>
 
                             <!-- Default Table -->
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th scope="col">RFC</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Calle</th>
-                                <th scope="col">Número</th>
-                                <th scope="col">Colonia</th>
-                                <th scope="col">Creación</th>
-                                <th scope="col">Status</th>
-                                <th>Acciones</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($requests as $req)
-                                <tr>
-                                    <th scope="row">{{ $req->rfc }}</th>
-                                    <td>{{ $req->nombre}}</td>
-                                    <td>{{ $req->calle}} </td>
-                                    <td>{{ $req->numeroCalle }}</td>
-                                    <td>{{ $req->colonia }}</td>
-                                    <td class="col-md-1" style="font-size: 14px">{{ $req->created_at->format('d-m-Y') }}</td>
-                                    <td class="col-md-1">
-                                        @if ($req->status === 1)
-                                            <h5 class="card-title m-0 p-0"><span class="badge text-white bg-secondary">Pendiente</span></h5>
-                                        @else
-                                            <h5 class="card-title m-0 p-0"><span class="badge text-white bg-success">Aprobado</span></h5>
-                                        @endif
+                            <div class="table table-responsive">
+                                <table class="table table-responsive" cellspacing="0" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">RFC</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Calle</th>
+                                            <th scope="col">Número</th>
+                                            <th scope="col">Colonia</th>
+                                            <th scope="col">Creación</th>
+                                            <th scope="col">Status</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($requests as $req)
+                                            <tr>
+                                                <th scope="row">{{ $req->rfc }}</th>
+                                                <td>{{ $req->nombre }}</td>
+                                                <td>{{ $req->calle }} </td>
+                                                <td>{{ $req->numeroCalle }}</td>
+                                                <td>{{ $req->colonia }}</td>
+                                                <td class="col-md-1" style="font-size: 14px">
+                                                    {{ $req->created_at->format('d-m-Y') }}</td>
+                                                <td class="col-md-1">
+                                                    @if ($req->status === 1)
+                                                        <h5 class="card-title m-0 p-0"><span
+                                                                class="badge text-white bg-secondary">Pendiente</span></h5>
+                                                    @else
+                                                        <h5 class="card-title m-0 p-0"><span
+                                                                class="badge text-white bg-success">Aprobado</span></h5>
+                                                    @endif
 
 
-                                    </td>
-                                    <td class="col-md-1">
-                                        <div class="btn-group mb-1">
-                                            <button type="button" class="btn btn-outline-success dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
-                                                <span class="sr-only">
-                                                    <i class="bi bi-menu-button-wide-fill"></i>
-                                                </span>
-                                            </button>
+                                                </td>
+                                                <td class="col-md-1">
+                                                    <div class="btn-group mb-1">
+                                                        <button type="button"
+                                                            class="btn btn-outline-success dropdown-toggle dropdown-toggle-split"
+                                                            data-bs-toggle="dropdown" aria-haspopup="true"
+                                                            aria-expanded="false" data-display="static">
+                                                            <span class="sr-only">
+                                                                <i class="bi bi-menu-button-wide-fill"></i>
+                                                            </span>
+                                                        </button>
 
-                                            <div class="dropdown-menu">
-                                                <a class="dropdown-item" href="{{ url(env('admin').'/empresas/proveedores/ver/'.$req->id) }}">Ver</a>
-                                                <a class="dropdown-item" href="{{ url(env('admin').'/empresas/proveedores/status/'.$req->id) }}">Activar/Desactivar</a>
-                                                <a class="dropdown-item" href="{{ url(env('admin').'/empresas/proveedores/usuarios/ver/'.$req->id) }}">Ver Usuários</a>
+                                                        <div class="dropdown-menu">
+                                                            <a class="dropdown-item"
+                                                                href="{{ url(env('admin') . '/empresas/proveedores/ver/' . $req->id) }}">Ver</a>
+                                                            <a class="dropdown-item"
+                                                                href="{{ url(env('admin') . '/empresas/proveedores/status/' . $req->id) }}">Activar/Desactivar</a>
+                                                            <a class="dropdown-item"
+                                                                href="{{ url(env('admin') . '/empresas/proveedores/usuarios/ver/' . $req->id) }}">Ver
+                                                                Usuários</a>
 
 
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        <!-- End Default Table Example -->
-                      </div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- End Default Table Example -->
+                        </div>
                     </div>
                 </div>
 
 
 
-                @if(count($requests)<1)
-                <div class="d-flex align-items-center flex-column py-6">
-                    <div>
-                        No hay solocitudes...
+                @if (count($requests) < 1)
+                    <div class="d-flex align-items-center flex-column py-6">
+                        <div>
+                            No hay solocitudes...
+                        </div>
                     </div>
-                </div>
                 @endif
             </div>
 
@@ -103,6 +115,4 @@
         <!--end container-->
     </section>
     <!--end section-->
-
-
 @endsection
