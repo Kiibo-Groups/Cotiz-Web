64a688db9d4f71797e488d6d67032474fd80b8c3
Jair Quinto <tokinmym2@gmail.com>
2023-09-04 14:52:40 -0500

    Responsive
    Signed-off-by: Jair Quinto <tokinmym2@gmail.com>

--- resources/views/proveedor/buzon/form.blade.php
+++ resources/views/proveedor/buzon/form.blade.php
@@ -19,7 +19,7 @@
                                 <input type="hidden" class="form-control slug-title"   name="admin_id" value="{{ $user }}">
                                 <input type="hidden" class="form-control slug-title"   name="origen" value="{{ $origen }}">
 
-                                    <div class="col-lg-12 form-floating mb-3">
+                                    <div class="col-lg-12  mb-3">
 
                                     <label for="provider" class="form-label">Proveedor</label>
                                     <select name="prove_id" id="prove_id" class="form-select js-example-basic-single" required>
@@ -35,7 +35,7 @@
                                     <textarea class="form-control" placeholder="Escribe una breve descripcion" id="descripcion" name="descripcion" required>{{ $data->description }}</textarea>
                                 </div>
 
-                                <div class="col-lg-12 form-floating mb-3">
+                                <div class="col-lg-12  mb-3">
                                     <label for="documento" class="form-label">Documento</label>
                                     <input type="file" accept=".png, .jpg, .jpeg, .pdf"  class="form-control slug-title"  id="documento" name="documento" required>
                                 </div>
